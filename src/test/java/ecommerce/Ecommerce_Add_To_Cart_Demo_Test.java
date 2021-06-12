941174da2d1dbd08c794b737f15f02b99d6d83ff
Mohammad Iqbal <Mohammad.Iqbal@QUALITEST.LOCAL>
2021-06-12 20:34:43 +0530

    Added gitigonre file and updated a test case

--- src/test/java/ecommerce/Ecommerce_Add_To_Cart_Demo_Test.java
+++ src/test/java/ecommerce/Ecommerce_Add_To_Cart_Demo_Test.java
@@ -30,37 +30,5 @@ public class Ecommerce_Add_To_Cart_Demo_Test extends HybridBase {
 		// cart page
 		Assert.assertEquals(item, cartPage.getItemText());
 		
-		
-		
-		//String scrollItem = "new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))\")";
-		/*
-		 * String scrollItem =
-		 * "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text(\""
-		 * + productNameText +"\").instance(0))";
-		 * driver.findElement(MobileBy.AndroidUIAutomator(scrollItem));
-		 */
-		
-		/*int visibleProductCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
-		
-		for(int i=0; i<visibleProductCount; i++) {
-			String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
-			if(productName.equalsIgnoreCase(item)) {
-				System.out.println("index number is : " + i);
-				driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
-				break;
-			}
-		}
-		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();*/
-		
-		
-		
-		
-		
-		
-		
-		//String itemName = driver.findElement(By.xpath("//android.widget.TextView[@resource-id='com.androidsample.generalstore:id/productName']")).getText();
-		
-		//System.out.println(itemName.equals(productNameText));
-		//validate the items added to cart
 	}
 }
